bbd3a8d33c34c1da1f6f9ae923ede55b72a37781
blewis7 <91699314+blewis7@users.noreply.github.com>
2021-12-02 19:07:23 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -7,25 +7,24 @@ app.config['SECRET_KEY'] = 'brockisgood'
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-responses = []
-
 
 @app.route('/')
 def homepage():
     '''The homepage needs to contain the title of the survey, instructions, and a start button'''
     return render_template('homepage.html', survey=survey)
 
-@app.route('/begin')
+@app.route('/begin', methods=['POST'])
 def begin_survey():
     '''A place to reset responses to empty and then redirect to the first question'''
-    if (len(responses) > len(survey.questions)):
-        responses.clear()
+    session['responses'] = []
     return redirect('/question/0')
 
 
 @app.route('/question/<int:num>')
 def questions(num):
     '''Display current question and possible choices'''
+
+    responses = session.get('responses')
 
     if len(responses) != num:
         flash('Invalid question')
@@ -35,11 +34,13 @@ def questions(num):
     return render_template('question.html', question=question, num=num)
 
 
-@app.route('/answer')
+@app.route('/answer', methods=['POST'])
 def handle_question():
     '''Take the answer and append it to responses'''
-    choice = request.args['answer']
+    responses = session['responses']
+    choice = request.form['answer']
     responses.append(choice)
+    session['responses'] = responses
 
     if (len(responses) == len(survey.questions)):
         return redirect('/complete')
@@ -49,8 +50,7 @@ def handle_question():
 
 @app.route('/complete')
 def finished():
-    answers = responses
-    responses.clear()
+    '''Survey is complete'''
     return render_template('complete.html')
 
     
